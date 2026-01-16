47d7c089c2ed4168bc40b5c19357ceb0e6584ae2
Chinemezee <chinemezenjoku@gmail.com>
2026-01-16 22:10:21 +0100

    added ai agent

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ from nba_api.stats.endpoints import playergamelog
 import pandas as pd
 import json
 import os
+from google import genai
+from dotenv import load_dotenv
+from pathlib import Path
+from dotenv import dotenv_values
+from google.genai import types
 import time
 
-nba_players = players.find_players_by_last_name("Harden")
+
+load_dotenv()
+print(os.getenv("GEMINI_API"))
+nba_player_name = "Jalen Williams"
+nba_players = players.find_players_by_full_name(f"{nba_player_name}")
 player_id = nba_players[0]["id"]
 print(player_id)
 
@@ -23,10 +32,26 @@ data.append(nba_players)
 with open(file_path, "w", encoding="utf-8") as file:
     json.dump(data, file, indent=4)  
 
-'''career = playercareerstats.PlayerCareerStats(player_id=player_id)   
+career = playercareerstats.PlayerCareerStats(player_id=player_id)   
 df = career.get_data_frames()
-print(df) '''   
+print(df)    
 
 gamelog = playergamelog.PlayerGameLog(player_id=player_id, season="2025-26")
-df1 = gamelog.get_data_frames()[0]["PTS"]
-print(df1)
+df1 = gamelog.get_data_frames()[0]
+#print(df1)
+
+client = genai.Client(api_key=os.getenv("GEMINI_API"))
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview", 
+    contents=[#f"write out the numbers in PTS column in {df1}"
+              f"The numbers in PTS column in {df1} are the number of points made by nba player, {nba_player_name} from most recent game to the last"
+              f"calculate the ppg and predict what the number of points in the next game would be"
+              f"The numbers in AST column in {df1} are the number of assists made by nba player, {nba_player_name} from most recent game to the last"
+              f"calculate the apg and predict what the number of assists in the next game would be"
+              ],
+    config=types.GenerateContentConfig(
+        system_instruction="You are a professional NBA analyst ."     
+    )     
+)
+print(response.text)
